c8499718dce20f7cd9c4ac73849fd103689f955a
IbrahimIddrisuRashida <ibrahimiddrisurashida@gmail.com>
2021-04-08 12:21:27 +0000

    second express commit

--- server.js
+++ server.js
@@ -1,20 +1,47 @@
-const express = require("express")
+const express = require ("express")
+const mechandise = require("./store")
 
 const app = express()
 
-const port = 5000;
 
-app.get("/", (req,res)=>{
-    res.send("welcome to fashion world")
-})
-app.post("/api/post_req", (req,res)=>{
-    res.send("welcome to my fashion world")
-})
-app.delete("/api/del_req", (req,res)=>{
-    res.send("welcome to my fashion world")
-})
-app.put("/api/put_req", (req,res)=>{
-    res.send("welcome to my fashion world")
+
+// body parser to allow updating, thus, PUT request
+app.use(express.json())
+
+
+
+// get all mechandise
+app.get("/api/v3/mech", (req, res)=> {
+    res.json(mechandise);
 })
 
-app.listen(port, ()=>console.log(`server running ${port}`))
+// single farmer
+app.get("/api/v3/mech/:name", (req, res)=> {
+    res.json(mechandise.filter((mech)=>mech.name === req.params.name));
+})
+
+// delete a mechandise
+app.delete("/api/v3/mech/:name", (req, res)=> {
+    res.json(mechandise.filter((mech)=>mech.name !== req.params.name));
+})
+
+// updating a mechandise 
+app.put("/api/v3/mech/:name", (req, res)=> {
+    const mechFound= mechandise.some((mech)=> mech.name === req.params.name);
+    mechFound && 
+        mechandise.forEach((mech)=> {
+            mech.name===req.body.name &&
+            (mech.name = req.body.name,
+            mech.type = req.body.type,
+            mech.cost = req.body.cost)
+        });
+        res.json(mechandise);
+})
+
+ 
+
+
+
+const PORT = 5000
+
+app.listen(PORT, ()=> console.log(`Server started on port ${PORT}`) )
---
+++ store.js
@@ -0,0 +1,13 @@
+const mechandise= [
+    {name:"shirt", type:"long sleeve", cost:"GH65", id:"050"},
+    {name:"shoes", type:"classic leather", cost:"GH150", id:"020"},
+    {name:"shorts", type:"beach wear", cost:"GH40", id:"030"},
+    {name:"socks", type:"designer", cost:"GH10", id:"040"},
+    {name:"inner", type:"boxer", cost:"GH30", id:"060"},
+    {name:"scarf", type:"gucci", cost:"GH20", id:"070"},
+    {name:"watch", type:"rolex", cost:"GH70", id:"080"},
+    {name:"chain", type:"gold", cost:"GH50", id:"090"}
+    
+];
+
+module.exports = mechandise;
